82e4c02727383e166a94e1aa63d0bee1dc18be53
Igor Pechenkin <ipechenkin@gmail.com>
2021-10-05 22:56:00 +0300

    add style for search block

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,7 @@
+import styles from './Search.module.css'
 import api from "../../api/api";
-
+import searchIcon from '../../assets/images/searc-icon.png';
+ // TODO задезейблить форму, елси пустая строка
 const Search = (props) => {
   const onInputChange = (e) => {
     const value = e.target.value;
@@ -28,32 +30,36 @@ const Search = (props) => {
   };
 
   return (
-    <div>
-      <h1>Блок поиска</h1>
-      <form onSubmit={onFormSubmit}>
-        <p>
-          <label htmlFor="search-input">Строка поиска</label>
+    <div className={styles.searchBlock}>
+      <form className={styles.searchForm} onSubmit={onFormSubmit}>
+        <h1>Search for books</h1>
+        <p className={styles.searchInput}>
           <input type="text" id="search-input" value={props.searchString} onChange={onInputChange} />
+          <button type="submit">
+            <img src={searchIcon} width="20" alt="Кнопка поиска"/>
+          </button>
         </p>
-        <p>
-          <label htmlFor="categories-select">Categories</label>
-          <select name="categories" id="categories-select" value={props.category} onChange={onCategoryChange}>
-            <option value="all">all</option>
-            <option value="art">art</option>
-            <option value="biography">biography</option>
-            <option value="computers">computers</option>
-            <option value="history">history</option>
-            <option value="medical">medical</option>
-            <option value="poetry">poetry</option>
-          </select>
-        </p>
-        <p>
-          <label htmlFor="sorting-select">Sorting by</label>
-          <select name="sorting" id="sorting-select" value={props.sortBy} onChange={onSortByChange}>
-            <option value="relevance">relevance</option>
-            <option value="newest">newest</option>
-          </select>
-        </p>
+        <div className={styles.wrapParams}>
+          <p className={styles.searchParam}>
+            <label htmlFor="categories-select">Categories</label>
+            <select name="categories" id="categories-select" value={props.category} onChange={onCategoryChange}>
+              <option value="all">all</option>
+              <option value="art">art</option>
+              <option value="biography">biography</option>
+              <option value="computers">computers</option>
+              <option value="history">history</option>
+              <option value="medical">medical</option>
+              <option value="poetry">poetry</option>
+            </select>
+          </p>
+          <p className={styles.searchParam}>
+            <label htmlFor="sorting-select">Sorting by</label>
+            <select name="sorting" id="sorting-select" value={props.sortBy} onChange={onSortByChange}>
+              <option value="relevance">relevance</option>
+              <option value="newest">newest</option>
+            </select>
+          </p>
+        </div>
 
       </form>
     </div>
