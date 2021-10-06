4155087ef22b99e91d3e3a2d037f121de55bfec7
Igor Pechenkin <ipechenkin@gmail.com>
2021-10-06 13:33:00 +0300

    refactor api

--- src/api/api.js
+++ src/api/api.js
@@ -1,80 +1,50 @@
 import axios from 'axios';
 
+const mappingDataItem = (data) => {
+  const {
+    id,
+    volumeInfo: {
+      title = null,
+      authors = null,
+      categories = null,
+      description = null,
+      imageLinks: {
+        smallThumbnail = null,
+        thumbnail = null
+      } = {
+        smallThumbnail: null,
+        thumbnail: null
+      }
+    }
+  } = data;
+
+  return {
+    id,
+    title,
+    authors,
+    categories,
+    description,
+    imageLinks: {
+      smallThumbnail,
+      thumbnail
+    }
+  };
+};
+
 const axiosInstance = axios.create({
   baseURL: 'https://www.googleapis.com/books/v1/volumes',
 });
 
-const api = { // TODO обе загрузки одно и то же. убрать дублирование
+const api = {
   getBooks(searchString, sortBy, maxResults, startIndex = 0) {
     return axiosInstance
       .get(`?q=${searchString}&orderBy=${sortBy}&startIndex=${startIndex}&maxResults=${maxResults}`) // +subject:${props.category}
-      .then(response => {
-        const items = response.data.items.map((item) => {
-          const {
-            id,
-            volumeInfo: {
-              title = null,
-              authors = null,
-              categories = null,
-              description = null,
-              imageLinks: {
-                smallThumbnail = null,
-                thumbnail = null
-              } = {
-                smallThumbnail: null,
-                thumbnail: null
-              }
-            }
-          } = item;
-          return {
-            id,
-            title,
-            authors,
-            categories,
-            description,
-            imageLinks: {
-              smallThumbnail,
-              thumbnail
-            }
-          };
-        });
-
-        return items;
-      });
+      .then(response => response.data.items.map((item) => mappingDataItem(item)));
   },
   getBook(id) {
     return axiosInstance
       .get(`${id}`)
-      .then(response => {
-        const {
-          id,
-          volumeInfo: {
-            title = null,
-            authors = null,
-            categories = null,
-            description = null,
-            imageLinks: {
-              smallThumbnail = null,
-              thumbnail = null
-            } = {
-              smallThumbnail: null,
-              thumbnail: null
-            }
-          }
-        } = response.data;
-
-        return {
-          id,
-          title,
-          authors,
-          categories,
-          description,
-          imageLinks: {
-            smallThumbnail,
-            thumbnail
-          }
-        };
-      });
+      .then(response => mappingDataItem(response.data));
   }
 };
 
